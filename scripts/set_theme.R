f0cfe29530e9cfc6e30320750b17655b76ef119e
skhiggins <sean.higgins@kellogg.northwestern.edu>
2020-06-11 11:59:30 -0400

    Add set_theme.R and set_theme_reprex.R

---
+++ scripts/set_theme.R
@@ -0,0 +1,82 @@
+# Set theme for graphs
+# Sean Higgins
+
+set_theme <- function(
+  theme = theme_classic(),
+  size = 14, 
+  title_size = size,
+  title_hjust = 0.5,
+  y_title_size = size,
+  x_title_size = size,
+  y_title_margin = NULL,
+  x_title_margin = NULL,
+  y_text_size = size,
+  x_text_size = size,
+  y_text_color = "black",
+  x_text_color = "black",
+  y_title_color = "black", 
+  x_title_color = "black",
+  legend_text_size = size,
+  legend_position = "none",
+  plot_title_position = NULL,
+  plot_margin = theme_classic()$plot.margin,
+  axis_title_y_blank = FALSE, # to fully left-align
+  aspect_ratio = NULL
+) {
+  # Size
+  size_ <- str_c("size = ", size) # this argument always included
+  
+  if (is.na(y_title_size)) {
+    y_title <- "element_blank()"
+  } else {
+    
+    # y-title margin
+    if (!is.null(y_title_margin)) y_title_margin_ <- str_c("margin = margin(", y_title_margin, ")")
+    else y_title_margin_ <- ""
+    
+    
+    # y-title color
+    if (!is.null(y_title_color)) y_title_color_ <- str_c("color = '", y_title_color, "'")
+    else y_title_color_ <- ""
+    
+    # create y_title
+    y_title <- str_c("element_text(", size_, ",", y_title_margin_, ",", y_title_color_, ")")
+    
+  }
+  if (is.na(x_title_size)) {
+    x_title <- "element_blank()"
+  }
+  else {
+    # x-title margin
+    if (!is.null(x_title_margin)) x_title_margin_ <- str_c("margin = margin(", x_title_margin, ")")
+    else x_title_margin_ <- ""
+    
+    # x-title color
+    if (!is.null(x_title_color)) x_title_color_ <- str_c("color = '", x_title_color, "'")
+    else x_title_color_ <- ""
+    
+    # create x_title
+    x_title <- str_c("element_text(", size_, ",", x_title_margin_, ",", x_title_color_, ")")
+  }
+  
+  if (axis_title_y_blank) {
+    y_title <- "element_blank()" # overwrite what it was written as above
+  }
+  
+  theme + theme(
+    plot.title = element_text(size = title_size, hjust = title_hjust),
+    plot.title.position = plot_title_position,
+    axis.title.y = eval(parse(text = y_title)),
+    axis.title.x = eval(parse(text = x_title)),
+    axis.ticks = element_blank(),
+    axis.text.y = element_text(size = y_text_size, color = y_text_color),
+    axis.text.x = element_text(size = x_text_size, color = x_text_color),
+    axis.line = element_blank(), # manual axes
+    legend.key = element_rect(fill = "white"),
+    legend.text = element_text(size = legend_text_size),
+    legend.title = element_text(size = legend_text_size),
+    aspect.ratio = aspect_ratio,
+    plot.margin = plot_margin,
+    legend.position = legend_position
+  )
+}
---
+++ scripts/set_theme_reprex.R
@@ -0,0 +1,62 @@
+# Simple examples of using set_theme() to standardize graph formatting
+#  Sean Higgins
+
+library(tidyverse)
+library(here)
+
+# Read in set_theme function
+source(here::here("scripts", "set_theme.R"))
+
+# use the defaults
+mtcars %>% ggplot() + 
+  geom_point(aes(y = hp, x = wt)) + 
+  labs(y = "Horsepower", x = "Weight") +
+  theme_classic() + 
+  set_theme()
+ggsave(
+  here::here("graphs", "set_theme_defaults.eps"), 
+  width = 8, # important to explicitly set these arguments 
+  height = 4 # for reproducibility of graphs 
+    # (otherwise will depend on size of Rstudio plots pane)
+)
+
+# add x and y axes at 0, decrease plot margins,
+#  remove extra space below 0, 
+#  add some cushion to the right of the y-axis title and above x-axis title
+mtcars %>% ggplot() + 
+  geom_point(aes(y = hp, x = wt)) + 
+  labs(y = "Horsepower", x = "Weight") +
+  geom_hline(yintercept = 0) + 
+  geom_vline(xintercept = 0) +
+  scale_y_continuous(expand = expansion(mult = c(0, 0.01))) + # get rid of space below 0
+  scale_x_continuous(expand = expansion(mult = c(0, 0.01))) + # and at top/right of plot
+    # note that expand = c(0, 0) would remove any space beyond scale but 
+    # that would lead the points with highest x or y values to get partially cut off
+    # so expand = expansion(mult = c(0, 0.01)) expands top/right by 1%
+    # see https://ggplot2.tidyverse.org/reference/expansion.html
+  set_theme(
+    y_title_margin = "r = 5",
+    x_title_margin = "t = 5", 
+    plot_margin = unit(c(t = 2, r = 2, b = 2, l = 2), "pt")
+  )
+ggsave(
+  here::here("graphs", "set_theme_with_axes.eps"), 
+  width = 8, # important to explicitly set these arguments 
+  height = 4 # for reproducibility of graphs 
+  # (otherwise will depend on size of Rstudio plots pane)
+)
+
+# Histogram
+mtcars %>% ggplot() + 
+  geom_histogram(aes(x = wt)) + 
+  labs(y = "Count", x = "Weight") + 
+  scale_y_continuous(expand = expansion(mult = c(0, 0.01))) +
+  geom_hline(yintercept = 0) + # add horizontal axis; 
+    # no vertical axis since histogram doesn't start at 0
+  set_theme()
+ggsave(
+  here::here("graphs", "set_theme_histogram.eps"), 
+  width = 8, # important to explicitly set these arguments 
+  height = 4 # for reproducibility of graphs 
+  # (otherwise will depend on size of Rstudio plots pane)
+)
